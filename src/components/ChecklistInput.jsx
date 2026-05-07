83afbaf94035e6f2bede1cb1d608ce74e21845a9
Mike3PO <miken5488@gmail.com>
2026-05-07 13:50:43 -0400

    Enter input

--- src/components/ChecklistInput.jsx
+++ src/components/ChecklistInput.jsx
@@ -10,6 +10,13 @@ export function ChecklistInput(props) {
                 handleAddItem(checklistInput, selectedTab)
                 setChecklistInput('')
             }}>
+            <TextField onKeyDown={(e) => {
+                if (!checklistInput) {return}
+                if (!(e.keyCode === 13)) {return}
+                handleAddItem(checklistInput, selectedTab)
+                setChecklistInput('')
+            }}
+            />
             <i className="fa-solid fa-plus"></i>
             </button>
         </div>
